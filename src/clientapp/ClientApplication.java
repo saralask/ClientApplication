6eabb0ef75fe504b85550820c27f0714afdf9672
Sarala Sharan <42351918+saralask@users.noreply.github.com>
2021-11-16 19:42:24 +0530

    api calls complete

--- src/clientapp/ClientApplication.java
+++ src/clientapp/ClientApplication.java
@@ -1,7 +1,12 @@
 package clientapp;
 
-import com.decryption.utility.*;
+import com.decryption.utility.GetDateOfExpiryImplementation;
+import com.decryption.utility.IsFeatureLicensedImplementation;
+import com.decryption.utility.ReadLocalLicenseImplementation;
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import utils.FileOperations;
 
+import java.io.File;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.*;
@@ -13,7 +18,7 @@ public class ClientApplication {
 
         Scanner scanner = new Scanner(System.in);
 
-        String filePath = "D:\\WORK\\Hackathon2021\\ClientAplication\\src\\text.txt";
+        String filePath = "D:\\WORK\\Hackathon2021\\ClientAplication\\resources\\license.txt";
         String requestURL = "http://localhost:8081/uploadLicense";
 
         List<String> generalLicensedFeatures = new ArrayList<>();
@@ -26,8 +31,8 @@ public class ClientApplication {
         proprietaryLicensedFeatures.add("SyncedTuning");
         proprietaryLicensedFeatures.add("PlayTogether");
 
-        Map<String,Boolean> featureAndItsVisibility = IsFeatureLicensedImplementation.isFeatureLicensed(filePath, proprietaryLicensedFeatures);
-        for(boolean value : featureAndItsVisibility.values()){
+        Map<String, Boolean> featureAndItsVisibility = IsFeatureLicensedImplementation.isFeatureLicensed(filePath, proprietaryLicensedFeatures);
+        for (boolean value : featureAndItsVisibility.values()) {
             if (value) {
                 isLicenseAvailable = true;
                 break;
@@ -37,24 +42,45 @@ public class ClientApplication {
         //clean the license file
         System.out.println("Hi Welcome to Harman Licensing feature");
         System.out.println("The available features are " +
-                "\n1. Feature 1"+
-                "\n2. Feature 2"+
-                "\n3. Feature 3"+
+                "\n1. Feature 1" +
+                "\n2. Feature 2" +
+                "\n3. Feature 3" +
                 "\n4. AutoTuning" +
                 "\n5. SyncedTuning" +
                 "\n6. Play Together"
         );
 
-//        if(!isLicenseAvailable){
-//
-//
-//        }
 
-        ReadLocalLicenseImplementation.requestReadLicense(filePath, requestURL);
+        if (!isLicenseAvailable) {
+            System.out.println("Do you want to access the paid features y/n?");
+            String response = scanner.next();
 
+            if (response.equals("y")) {
+                File file = ReadLocalLicenseImplementation.requestReadLicense(filePath, requestURL);
+                FileOperations.unZipFile(file);
+                isLicenseAvailable = true;
+                Map<String, Boolean> featureMap = IsFeatureLicensedImplementation.isFeatureLicensed(filePath, proprietaryLicensedFeatures);
+                List<String> licensedFeatures = new ArrayList<>();
 
+                for(Map.Entry<String, Boolean> map : featureMap.entrySet()){
+                    if(map.getValue()) licensedFeatures.add(map.getKey());
+                }
 
+                Map<String, Date> timeToExpiryMap = GetDateOfExpiryImplementation.getLicenseExpiration(filePath,licensedFeatures);
 
+                //execute
+                for(Map.Entry<String, Date> map : timeToExpiryMap.entrySet()){
+                    System.out.println("The feature "+map.getValue()+" expires on "+map.getValue());
+                }
+
+                //get local time match with time to expiry and run those features
+            }
+
+            else {
+                // run(GLL)
+                // run(licensed features)
+            }
+        }
 
     }
 }
